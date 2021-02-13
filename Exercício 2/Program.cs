22af7d68019d0f84f881a07c558b3e9b99ef5484
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-13 18:40:20 -0300

    Add files via upload

---
+++ Exercício 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros;
+            
+
+            int n = int.Parse(Console.ReadLine());
+
+            numeros = new int[n];
+
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            for (int i =0;i<n;i++)
+            {
+                
+                numeros[i] = int.Parse(vetor[i]);
+            }
+
+            int c = 0;
+
+            Console.WriteLine();
+
+            for (int i=0;i<n;i++)
+            {
+                if(numeros[i]%2==0)
+                {
+                    Console.Write(numeros[i] + " ");
+                    c++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(c);
+        }
+    }
+}
